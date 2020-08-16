a7431e55958b242f89e230b4bc5e29c1c847706f
GORDY410 <reip14@gmail.com>
2020-08-15 22:10:22 -0500

    Agregué la interfáz del juego del gato y la funcionalidad de jugadores, faltando la funcionalidad de tirar por parte de cada jugador o computadora.

--- Ejercicios/behaivor.js
+++ Ejercicios/behaivor.js
@@ -1,12 +1,15 @@
 var reloj = new Vue({
 	el: '#reloj',
+
 	data: {
 		hora: ''
 	},
+
 	created: function() {
 		this.obtenerHora();
 		setInterval( this.obtenerHora, 1000 );
 	},
+
 	methods: {
 		obtenerHora: function(){
 			var tiempo = new Date();
@@ -38,12 +41,15 @@ var reloj = new Vue({
 
 var fechas = new Vue({
 	el: '#fechas',
+
 	data: {
 		fecha: ''
 	},
+
 	created: function(){
 		this.obtenerFecha();
 	},
+
 	methods: {
 		obtenerFecha: function(){
 			var tiempo = new Date();
@@ -62,13 +68,18 @@ var fechas = new Vue({
 
 var calculadora = new Vue({
 	el: '#calculadora',
+
 	data: {
 		pantallaSec: '',
 		pantallaPrin: '',
+		
 		numero1: 0,
 		numero2: 0,
-		operacion: ''
+		operacion: '',
+
+		vista: true
 	},
+
 	methods: {
 		obtenerNumero: function( numero ){
 			this.pantallaPrin = this.pantallaPrin + numero.toString();
@@ -129,6 +140,122 @@ var calculadora = new Vue({
 			this.numero1 = 0;
 			this.numero2 = 0;
 			this.operacion = '';
+		}
+	}
+});
+
+var gato = new Vue({
+	el: '#gato',
+
+	data: {
+		parrafoMensaje: '',
+		mensajeEstado: '',
+		mensajeTurno: '',
+
+		boton1: '',
+		boton2: '',
+		jugadores: false,
+		jugador1: true,
+		jugador2: false,
+		simbolos: [ , ],
+		tablero: [ ['', '', ''],
+			['', '', ''],
+			['', '', ''] ],
+
+		vista: false,
+		vistaPartida: false,
+		vistaMensaje: true,
+		vistaBotonesJug: true,
+		vistaBotonesSim: false
+	},
+
+	created: function(){
+		this.parrafoMensaje = 'Número de jugadores:';
+
+		this.boton1 = '1 Jugador';
+		this.boton2 = '2 Jugadores';
+
+		this.vista = false;
+	},
+
+	methods: {
+		seleccionarJugadores: function( opcion ){
+			/*
+			True: la partida será de 2 jugadores.
+			False: será de 1 jugador.
+			*/
+
+			if( opcion ){
+				this.parrafoMensaje = 'Jugador 1 elige tu simbolo:';
+				this.jugadores = true;
+			}else{
+				this.parrafoMensaje = 'Jugador elige tu simbolo:';
+			}
+
+			this.boton1 = 'X (Cruz)';
+			this.boton2 = 'O (Circulo)';
+
+			this.vistaBotonesJug = false;
+			this.vistaBotonesSim = true;
+		},
+
+		seleccionarSimbolo: function( opcion ){
+			/*
+			True: El jugador 1 elegió el simbolo 'O'.
+			False: EL juagdor 1 eligió el simbolo 'X'.
+			Al jugador 2 o a la computadora se le dará el otro simbolo.
+			*/
+
+			if( opcion ){
+				this.simbolos[0] = 'O';
+				this.simbolos[1] = 'X';
+			}else{
+				this.simbolos[0] = 'X';
+				this.simbolos[1] = 'O';
+			}
+
+			this.mensajeEstado = 'Jugador 1: ' + '"' + this.simbolos[0] + '"';
+			if( this.jugadores ){
+				this.mensajeEstado += ' Jugador 2: ' + '"' + this.simbolos[1] + '"';
+			}else{
+				this.mensajeEstado += ' Computadora: ' + '"' + this.simbolos[1] + '"';
+			}
+
+			this.mensajeTurno = 'Jugador 1 es tu turno';
+
+			this.vistaMensaje = false;
+			this.vistaPartida = true;
+		},
+
+		tirar: function(){
+
+			if( turno ){
+
+			}
+
+		}
+	}
+});
+
+var navegacion = new Vue({
+	el: '#navegacion',
+	methods: {
+		aparecerCalculadora: function(){
+			calculadora.vista = true;
+
+			gato.vista = false;
+		},
+
+		aparecerConvertidor: function(){
+			calculadora.vista = false;
+
+			gato.vista = false;
+		},
+
+		aparecerGato: function(){
+			calculadora.vista = false;
+
+			gato.vista = true;
 		}
 	}
 });
